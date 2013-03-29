232a64a008436c2ebfda58d3184343b658fde8aa
Dave Townsend <dtownsend@oxymoronical.com>
2013-03-28 17:15:40 -0700

    Implement additions to database through a mercurial hook.

---
+++ changegroup.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+project = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+if project not in sys.path:
+    sys.path.insert(0, project)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hgchangefeed.settings'
+
+from datetime import datetime
+
+from django.db import transaction
+
+from website.models import *
+
+def get_repository(url):
+    try:
+        return Repository.objects.get(url = url)
+    except Repository.DoesNotExist:
+        stripped = url
+        if url[-1] == "/":
+            stripped = url[:-1]
+        name = stripped.split("/")[-1]
+
+        repository = Repository(url = url, name = name)
+        repository.save()
+
+        root = Path(repository = repository, name = '', path = '')
+        root.save()
+
+        return repository
+
+def get_path(repository, path):
+    try:
+        return Path.objects.get(repository = repository, path = path)
+    except Path.DoesNotExist:
+        parts = path.rsplit("/", 1)
+        parent = get_path(repository, parts[0] if len(parts) > 1 else '')
+        result = Path(repository = repository, path = path, name = parts[-1], parent = parent)
+        result.save()
+        return result
+
+def add_change(change):
+    path = change.path
+    while path is not None:
+        pathchanges, created = PathChanges.objects.get_or_create(path = path, changeset = change.changeset)
+        pathchanges.changes.add(change)
+
+        path = path.parent
+
+@transaction.commit_on_success
+def hook(ui, repo, node, **kwargs):
+    repository = get_repository(kwargs["url"])
+
+    # All changesets from node to "tip" inclusive are part of this push.
+    rev = repo.changectx(node).rev()
+    tip = repo.changectx("tip").rev()
+
+    for i in xrange(rev, tip + 1):
+        changectx = repo.changectx(i)
+
+        changeset = Changeset(repository = repository,
+                              rev = changectx.rev(),
+                              hex = changectx.hex(),
+                              user = changectx.user(),
+                              date = datetime.utcfromtimestamp(changectx.date()[0]),
+                              description = changectx.description())
+        changeset.save()
+
+        parents = changectx.parents()
+
+        ui.status("%s\n" % changeset)
+
+        for file in changectx.files():
+            path = get_path(repository, file)
+
+            type = "A"
+            if not file in changectx:
+                type = "R"
+            elif any([file in c for c in parents]):
+                type = "M"
+
+            change = Change(changeset = changeset, path = path, type = type)
+            change.save()
+
+            add_change(change)
+
+            ui.status("%s\n" % change)
+
+        ui.status("\n")
+
+    return False
--- models.py
+++ models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+CHANGE_TYPES = (
+    ("A", "Addition"),
+    ("M", "Modification"),
+    ("D", "Deleted"),
+)
+
+class Repository(models.Model):
+    url = models.TextField(unique = True)
+    name = models.TextField(unique = True)
+
+    def root(self):
+        return paths.objects.get(repository = self, parent = None)
+
+    def __unicode__(self):
+        return self.name
+
+class Path(models.Model):
+    name = models.TextField()
+    path = models.TextField()
+    parent = models.ForeignKey('self', related_name = "children", null = True)
+    repository = models.ForeignKey(Repository)
+
+    def __unicode__(self):
+        return self.path
+
+    class Meta:
+        unique_together = (("repository", "path"), ("parent", "name"))
+
+class Changeset(models.Model):
+    repository = models.ForeignKey(Repository)
+    rev = models.IntegerField()
+    hex = models.CharField(max_length = 40)
+    user = models.TextField()
+    date = models.DateTimeField()
+    description = models.TextField()
+    paths = models.ManyToManyField(Path, through = "PathChanges")
+
+    def url(self):
+        return "%srev/%s" % (self.repository.url, self.hex)
+
+    def __unicode__(self):
+        return self.hex
+
+    class Meta:
+        unique_together = (("repository", "rev"), ("repository", "hex"))
+
+class Change(models.Model):
+    changeset = models.ForeignKey(Changeset)
+    path = models.ForeignKey(Path)
+    type = models.CharField(max_length=1, choices=CHANGE_TYPES)
+
+    def __unicode__(self):
+        return "%s %s" % (self.type, self.path.path)
+
+    class Meta:
+        unique_together = ("changeset", "path")
+
+class PathChanges(models.Model):
+    changeset = models.ForeignKey(Changeset)
+    path = models.ForeignKey(Path)
+    changes = models.ManyToManyField(Change)
+
+    class Meta:
+        unique_together = ("changeset", "path")
+
