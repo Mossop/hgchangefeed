470a0a55e3db85c2477a510e6f75c17170140509
Dave Townsend <dtownsend@oxymoronical.com>
2013-03-29 23:12:48 -0700

    Support restricting changesets by change type.

--- views.py
+++ views.py
@@ -2,6 +2,12 @@ from django.shortcuts import get_object_or_404, render
 
 from website.models import *
 
+TYPEMAP = {
+    "added": "A",
+    "removed": "R",
+    "modified": "M",
+}
+
 def index(request):
     repositories = Repository.objects.order_by('name')
     context = { "repositories": repositories }
@@ -10,10 +16,16 @@ def index(request):
 def path(request, repository_name, path_name):
     repository = get_object_or_404(Repository, name = repository_name)
     path = get_object_or_404(Path, repository = repository, path = path_name)
+
+    changesets = Changeset.objects.filter(changes__pathlist__in = [path]).distinct()
+    if "types" in request.GET:
+        types = [TYPEMAP[t] for t in request.GET["types"].split(",")]
+        changesets = [c for c in changesets if c.changes.filter(type__in = types).count() > 0]
+
     context = {
       "repository": repository,
       "path": path,
-      "changesets": Changeset.objects.filter(changes__pathlist__in = [path]).distinct()[:100],
+      "changesets": changesets[:100],
       "paths": path.children.all(),
     }
     return render(request, "path.html", context)
