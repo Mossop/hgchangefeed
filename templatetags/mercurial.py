32b3416b04f829a0865e3a22c2912a5cc01ba3ad
Dave Townsend <dtownsend@oxymoronical.com>
2013-03-29 18:52:37 -0700

    Link bug urls in changeset descriptions.

--- templatetags/mercurial.py
+++ templatetags/mercurial.py
@@ -1,5 +1,9 @@
+import re
+
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
 
 register = template.Library()
 
@@ -7,3 +11,22 @@ register = template.Library()
 @stringfilter
 def summarise(value):
     return value.split("\n")[0]
+
+@register.filter(needs_autoescape = True)
+@stringfilter
+def bugzilla(value, autoescape = None):
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
+
+    result = ""
+    bugs = re.finditer('\\b(?:BUG )?(\\d{4,7})\\b', value, re.IGNORECASE)
+    last = 0
+    for bug in bugs:
+        result = "%s%s<a href=\"https://bugzilla.mozilla.org/show_bug.cgi?id=%s\">%s</a>" % (result, esc(value[last:bug.start()]), bug.group(1), esc(bug.group()))
+        last = bug.end()
+
+    result = result + esc(value[last:])
+
+    return mark_safe(result)
