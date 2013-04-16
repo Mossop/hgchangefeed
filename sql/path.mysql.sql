a807c3628a161cb20a306b5111c0486324105e2f
Dave Townsend <dtownsend@oxymoronical.com>
2013-04-16 11:25:56 -0700

    Fix indexing for InnoDB

--- sql/path.mysql.sql
+++ sql/path.mysql.sql
@@ -1,2 +1,2 @@
-CREATE INDEX `website_path_path` ON `website_path` (`path`(300));
-CREATE INDEX `website_path_name` ON `website_path` (`name`(300));
+CREATE INDEX `website_path_path` ON `website_path` (`path`(200));
+CREATE INDEX `website_path_name` ON `website_path` (`name`(200));
