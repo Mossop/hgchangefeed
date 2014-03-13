b554af4c8a831b4a67969bdf56427a467f1456e6
Dave Townsend <dtownsend@oxymoronical.com>
2014-03-13 10:55:52 -0700

    Switch to using the low-level feed framework for better view control.

--- website/feeds.py
+++ website/feeds.py
@@ -2,9 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from django.contrib.syndication.views import Feed
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
+from django.utils import feedgenerator
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 from website.models import *
 
@@ -14,59 +16,37 @@ TYPEMAP = {
     "modified": "M",
 }
 
-class PathFeedRequest(object):
-    def __init__(self, repository, path, types):
-        self.repository = repository
-        self.path = path
-        self.types = types
+def path(request, repository_name, path_name):
+    path = Path.get_by_path(path_name)
+    repository = get_object_or_404(Repository, name = repository_name, paths = path, hidden = False)
 
-class ChangesetItem(object):
-    def __init__(self, repository, changeset):
-        self.repository = repository
-        self.changeset = changeset
+    queryparams = {
+        "pushes__push__repository": repository,
+    }
 
-class PathFeed(Feed):
-    description_template = 'pathfeed.html'
+    if path.parent is not None:
+        queryparams["changes__path__ancestors__ancestor"] = path
 
-    def get_object(self, request, repository_name, path_name):
-        path = Path.get_by_path(path_name)
-        repository = get_object_or_404(Repository, name = repository_name, paths = path, hidden = False)
-        types = None
-        if "types" in request.GET:
-            types = [TYPEMAP[t] for t in request.GET["types"].split(",")]
-        return PathFeedRequest(repository, path, types)
+    if "types" in request.GET:
+        types = [TYPEMAP[t] for t in request.GET["types"].split(",")]
+        queryparams["changes__type__in"] = types
 
-    def title(self, req):
-        return "Changes in %s %s" % (req.repository, req.path)
+    changesets = Changeset.objects.filter(**queryparams).distinct().order_by("-pushes__push__push_id", "-pushes__index")
+    changesets = list(changesets[:20])
 
-    def link(self, req):
-        return reverse('path', args=[req.repository, req.path])
+    feed = feedgenerator.Rss201rev2Feed(
+        title = "Changes in %s %s" % (repository, path),
+        description = "Changes recently made to %s in %s" % (path, repository),
+        link = reverse('path', args=[repository, path])
+    )
 
-    def description(self, req):
-        return "Changes recently made to %s in %s" % (req.path, req.repository)
+    for changeset in changesets:
+        feed.add_item(
+            title = "Changeset %s" % changeset,
+            description = render_to_string("pathfeed.html", { "changeset": changeset }),
+            link = "%srev/%s" % (repository.url, changeset),
+            author_name = changeset.author.split(" <")[0],
+            pubdate = changeset.localdate,
+        )
 
-    def items(self, req):
-        queryparams = {
-            "pushes__push__repository": req.repository
-        }
-
-        if req.path.parent is not None:
-            queryparams["changes__path__ancestors__ancestor"] = req.path
-
-        if req.types is not None:
-            queryparams["changes__type__in"] = req.types
-
-        changesets = Changeset.objects.filter(**queryparams).distinct().order_by("-pushes__push__push_id", "-pushes__index")[:20]
-        return [ChangesetItem(req.repository, c) for c in changesets]
-
-    def item_title(self, item):
-        return "Changeset %s" % item.changeset
-
-    def item_author_name(self, item):
-        return item.changeset.author.split(" <")[0]
-
-    def item_pubdate(self, item):
-        return item.changeset.localdate
-
-    def item_link(self, item):
-        return "%srev/%s" % (item.repository.url, item.changeset)
+    return HttpResponse(feed.writeString('UTF-8'), content_type="application/rss+xml; charset=utf-8")
--- website/urls.py
+++ website/urls.py
@@ -10,6 +10,6 @@ from website import feeds
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<repository_name>[^/]+)/file/(?P<path_name>.*)$', views.path, name='path'),
-    url(r'^(?P<repository_name>[^/]+)/feed/(?P<path_name>.*)$', feeds.PathFeed(), name='feed'),
+    url(r'^(?P<repository_name>[^/]+)/feed/(?P<path_name>.*)$', feeds.path, name='feed'),
     url(r'^(?P<repository_name>[^/]+)/rev/(?P<changeset_id>.*)$', views.changeset, name='changeset'),
 )
