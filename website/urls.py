4dac6c208045002672c8797bb2aa15015615e540
Dave Townsend <dtownsend@oxymoronical.com>
2014-05-05 11:45:04 -0700

    Disallow search bots.

--- website/urls.py
+++ website/urls.py
@@ -3,12 +3,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from website import views
 from website import feeds
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^(?P<repository_name>[^/]+)/file/(?P<path_name>.*)$', views.path, name='path'),
     url(r'^(?P<repository_name>[^/]+)/feed/(?P<path_name>.*)$', feeds.path, name='feed'),
     url(r'^(?P<repository_name>[^/]+)/rev/(?P<changeset_id>.*)$', views.changeset, name='changeset'),
