83e47f0c5d1ee252d7340a281f93754b41132b47
Dave Townsend <dtownsend@oxymoronical.com>
2015-01-28 21:16:44 -0800

    Fix ui.traceback calls.

--- website/management/command.py
+++ website/management/command.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import sys
+
 from django.core.management.base import BaseCommand
 
 WIDTH = 78
